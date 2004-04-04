f5a5f5b5176c682523c9b9f01e87683afcda7c2b
Brian Matzon <brian@matzon.dk>
2004-04-04 10:24:15 +0000

    added win32 cvs build

--- download.php
+++ download.php
@@ -1,6 +1,5 @@
 <?php
   require 'include/default.php';
-  require 'include/news.php';
 
   printHeader(null, null);
   printMenu("download");
@@ -12,6 +11,10 @@
     <p>
       Latest stable release is 0.8. Please download it here:<br>
       <a href="http://sourceforge.net/project/showfiles.php?group_id=58488&package_id=54362&release_id=192924" target="_blank">LWJGL 0.8 alpha</a>
+    </p>
+    <p>
+      Random Win32 CVS builds are provided here: <i>Please note that these builds may be more buggy than an official release, but will typically contain the newest features.</i><br>
+      <a href="http://matzon.dk/brian/lwjgl/builds/lwjgl-2004-03-31.rar" target="">lwjgl-2004-03-31.rar</a><br>
     </p>
     <p>
       Latest CVS tarball. Please download it here:<br>
