2bd256a3a95680ee618fbb404d660c06c2ea7ddd
Brian Matzon <brian@matzon.dk>
2004-09-06 21:54:50 +0000

    jnlp updated

--- jnlp/lwjgl-demo.php
+++ jnlp/lwjgl-demo.php
@@ -28,16 +28,25 @@ header("Content-type: application/x-java-jnlp-file");
   </resources> 
   <resources os="Windows">
     <j2se version="1.4+"/> 
-    <nativelib href="lwjgl-windows.jar"/>
-    <nativelib href="lwjglaudio-windows.jar"/>
-    <nativelib href="lwjglfmod3-windows.jar"/>";
+    <nativelib href="lwjgl_win32.jar"/>
+    <nativelib href="lwjglaudio_win32.jar"/>
+    <nativelib href="lwjglfmod3_win32.jar"/>";
   </resources> 
   <resources os="Linux" arch="i386">
     <j2se version="1.4+"/> 
-    <nativelib href="lwjgl-linux.jar"/>
-    <nativelib href="lwjglaudio-linux.jar"/>
-    <nativelib href="lwjglfmod3-linux.jar"/>";
+    <nativelib href="lwjgl_linux.jar"/>
+    <nativelib href="lwjglaudio_linux.jar"/>
+    <nativelib href="lwjglfmod3_linux.jar"/>";
   </resources>   
   <!--<property key="java.library.path" value="." />-->
-  <application-desc main-class="org.lwjgl.<?php echo substr($_SERVER["PATH_INFO"], 1, strlen($_SERVER["PATH_INFO"])-1); ?>"/>
+  <application-desc main-class="org.lwjgl.<?php echo substr($_SERVER["PATH_INFO"], 1, strlen($_SERVER["PATH_INFO"])-1); ?>">
+  <?php
+    if(isset($HTTP_GET_VARS['arguments'])) {
+      $arguments = $HTTP_GET_VARS['arguments'];
+      foreach ($arguments as $argument) {
+        echo "<argument>" . $argument . "</argument>\n";
+      }
+    }
+  ?>
+  </application-desc>
 </jnlp>
