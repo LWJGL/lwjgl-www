edd471477856ad6b134ed7c4ff7e5a10141fbed9
Brian Matzon <brian@matzon.dk>
2005-01-18 21:27:38 +0000

    updated links

--- links.php
+++ links.php
@@ -5,29 +5,39 @@
 	The following list of links are sites that either relate to our effort, or we just think are cool:
 </p>
 <ul>
+	<br>
+	<b>Game engines using LWJGL</b>
 	<li>
-		<a href="http://www.javagaming.org/" target="_blank">http://www.javagaming.org/</a><br/>
-		Site related to Java gaming.<br/><br/>
+		<a href="http://www.espresso3d.com/" target="_blank">http://www.espresso3d.com/</a><br/>
+		Espresso3D is a high performance real-time 3D engine for the Java(tm) programming language. E3D is not just a scene graph. It aims to be a complete solution for your application with OpenGL rendering, OpenAL audio, collision detection, input, and rendering support.<br/><br/>
 	</li>
 	<li>
-		<a href="http://www.puppygames.net/" target="_blank">http://www.puppygames.net/</a><br/>
-		First commercial game using LWJGL!<br/><br/>
-	</li>
-	<li>
-		<a href="http://www.tribaltrouble.com/" target="_blank">http://www.tribaltrouble.com/</a><br/>
-		Awesome new commercial game using LWJGL.<br/><br/>
-	</li>
-	<li>
-		<a href=" http://sourceforge.net/projects/spgl/" target="_blank"> http://sourceforge.net/projects/spgl/</a><br/>
-		Game library building on top of LWJGL.<br/><br/>
+		<a href="https://jge.dev.java.net/" target="_blank">https://jge.dev.java.net/</a><br/>
+		OctLight is a Java game engine that will provide all the tools necessary to create 3D massively multi player online games. It can be used for single player games as well, although that's the not the main focus. OctLight includes a scene graph API which is rendered using OpenGL. Built on top of this is a map and object framework which includes collision detection and visibility checking. In addition to this modules for networking, AI, GUI and persistence will be created.<br/><br/>
 	</li>
 	<li>
 		<a href="http://www.mojomonkeycoding.com/" target="_blank">http://www.mojomonkeycoding.com/</a><br/>
 		jME (or jMonkeyEngine) is an attempt at a fully functional game engine using the Java programming language.<br/><br/>
 	</li>
 	<li>
-		<a href="http://www.hardcode.de/jxinput/" target="_blank">http://www.hardcode.de/jxinput/</a><br/>
-		More feature complete input support for java.<br/><br/>
+		<a href="http://xith.org/" target="_blank">http://xith.org/</a><br/>
+		Xith3D is an open source 3D scenegraph for Java. It is designed to be leaner, more game orientated alternative to the Java3D scenegraph, yet using the same basic scenegraph structure as Java3D (ie. similar functioning TransformGroups?, Nodes, Views, etc).<br/><br/>
+	</li>
+	<br>
+	<b>Other LWJGL related libraries</b>
+	<li>
+		<a href="http://sourceforge.net/projects/spgl/" target="_blank">http://sourceforge.net/projects/spgl/</a><br/>
+		General Game library building on top of LWJGL. (Used in commercial games)<br/><br/>
+	</li>
+	<br>
+	<b>Game related sites</b>
+	<li>
+		<a href="http://www.javagaming.org/" target="_blank">http://www.javagaming.org/</a><br/>
+		Site related to Java gaming.<br/><br/>
+	</li>
+	<li>
+		<a href="http://www.gamedev.net/" target="_blank">http://www.gamedev.net/</a><br/>
+		Site related to game development.<br/><br/>
 	</li>
 </ul>
 
