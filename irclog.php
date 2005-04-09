918b66c862ab1d60d1a6b58912769dbff8b566d6
Brian Matzon <brian@matzon.dk>
2005-04-09 09:51:42 +0000

    updated irc log to format better

--- irclog.php
+++ irclog.php
@@ -3,10 +3,20 @@
 <h1>Last 100 lines of the IRC chat (<a href="irc://irc.freenode.net/lwjgl">#LWJGL IRC channel</a>)</h1>
 <p>
 <?php 
+
+	function formatIRCLine($line) { 
+		$date = sscanf($line, "%s %s");
+		$content = substr($line, strlen($date[0] . $date[1]) + 2);
+		
+		return "[" . $date[1] . " " . $content;
+	}
+
 	$body = htmlentities(file_get_contents("http://irc-stats.lwjgl.org/log.php"));
 	$lines = split("\n", $body);
 	foreach ($lines as $line) {
-  	print $line . "<br>\n";
+		if(strlen($line) > 0) {
+  		print formatIRCLine($line) . "<br>\n";
+		}
 	}	
 ?>
 </p>
