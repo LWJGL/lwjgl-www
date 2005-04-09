eeb9cd642f4a669e64a063ade2f1339e6b117f08
Brian Matzon <brian@matzon.dk>
2005-04-09 09:13:50 +0000

    no longer using PRE - broke layout on long lines

--- irclog.php
+++ irclog.php
@@ -2,8 +2,12 @@
 
 <h1>Last 100 lines of the IRC chat (<a href="irc://irc.freenode.net/lwjgl">#LWJGL IRC channel</a>)</h1>
 <p>
-<pre>
-<?php echo htmlentities(file_get_contents("http://irc-stats.lwjgl.org/log.php")); ?>
-</pre>
+<?php 
+	$body = htmlentities(file_get_contents("http://irc-stats.lwjgl.org/log.php"));
+	$lines = split("\n", $body);
+	foreach ($lines as $line) {
+  	print $line . "<br>\n";
+	}	
+?>
 </p>
 <? require('_include/footer.php'); ?>
