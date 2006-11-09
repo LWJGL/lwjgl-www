cb58dee5786a5191f5ebe6321299b3e172420dc5
Brian Matzon <brian@matzon.dk>
2006-11-09 20:43:55 +0000

    wrong version

--- download.php
+++ download.php
@@ -2,7 +2,7 @@
 
 <h1>Download</h1>
 <p>
-	Latest stable release is 1.0 beta 2. Please download it here:<br/>
-	<a href="https://sourceforge.net/project/showfiles.php?group_id=58488" target="_blank">LWJGL 1.0 beta 2</a>
+	Latest stable release is 1.0 beta 3. Please download it here:<br/>
+	<a href="https://sourceforge.net/project/showfiles.php?group_id=58488" target="_blank">LWJGL 1.0 beta 3</a>
 </p>
 <? require('_include/footer.php'); ?>
