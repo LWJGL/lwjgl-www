cba61ffd304fbb9fe6b4310bb03282b92e025e90
Ioannis Tsakpinis <iotsakp@gmail.com>
2014-12-28 19:50:34 +0200

    Link fixes

--- _include/header.php
+++ _include/header.php
@@ -14,15 +14,15 @@
 					<h2>LWJGL</h2>
 					<p>
          			<a href="index.php"<?=((strpos($_SERVER['SCRIPT_NAME'], 'index.php') !== false) ? ' class="activeItem"' : '');?>>Home</a><br/>
-					    <a href="/wiki/index.php?title=About_LWJGL">About</a><br/>
+					    <a href="http://wiki.lwjgl.org/index.php?title=About_LWJGL">About</a><br/>
 					    <a href="shop.php"<?=((strpos($_SERVER['SCRIPT_NAME'], 'shop.php') !== false) ? ' class="activeItem"' : '');?>>Shop</a><br/>
 					    <a href="donations.php"<?=((strpos($_SERVER['SCRIPT_NAME'], 'donations.php') !== false) ? ' class="activeItem"' : '');?>>Donations</a><br/>
 					    <a href="download.php"<?=((strpos($_SERVER['SCRIPT_NAME'], 'download.php') !== false) ? ' class="activeItem"' : '');?>>Download</a><br/>
-					    <a href="/wiki/index.php?title=Main_Page#Getting_started">Installation</a><br/>
-					    <a href="/wiki/index.php?title=Main_Page#Getting_started">Documentation</a><br/>
-					    <a href="/wiki" target="_blank">Wiki</a><br/>
+					    <a href="http://wiki.lwjgl.org/index.php?title=Main_Page#Getting_started">Installation</a><br/>
+					    <a href="documentation.php">Documentation</a><br/>
+					    <a href="http://wiki.lwjgl.org" target="_blank">Wiki</a><br/>
 					    <a href="http://forum.lwjgl.org" target="_blank">Forum</a><br/>
-					    <a href="/wiki/index.php?title=General_FAQ">FAQ</a><br/>
+					    <a href="http://wiki.lwjgl.org/index.php?title=General_FAQ">FAQ</a><br/>
 					    <a href="projects.php"<?=((strpos($_SERVER['SCRIPT_NAME'], 'projects.php') !== false) ? ' class="activeItem"' : '');?>>Projects</a><br/>
 					    <a href="demos.php"<?=((strpos($_SERVER['SCRIPT_NAME'], 'demos.php') !== false) ? ' class="activeItem"' : '');?>>Demos</a><br/>
 					    <a href="license.php"<?=((strpos($_SERVER['SCRIPT_NAME'], 'license.php') !== false) ? ' class="activeItem"' : '');?>>License</a><br/>
@@ -37,7 +37,7 @@
 					</p>
 					<h2>Miscellaneous</h2>
 					<p>
-					    <a href="/wiki/index.php?title=Main_Page#External_Links_and_Resources">Links</a><br/>
+					    <a href="http://wiki.lwjgl.org/index.php?title=Main_Page#External_Links_and_Resources">Links</a><br/>
 					    <a href="changelog.php"<?=((strpos($_SERVER['SCRIPT_NAME'], 'changelog.php') !== false) ? ' class="activeItem"' : '');?>>Changelog</a><br/>
 					    <a href="http://echelog.com/logs/browse/lwjgl/" target="_blank">IRC Chatlog</a><br/>
 					</p>
